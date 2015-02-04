cea606c9aa382de16d428d0086284484096db66b
Jett <whackashoe@gmail.com>
2015-02-04 11:47:47 -0600

    settings mods

--- main.cpp
+++ main.cpp
@@ -7,16 +7,16 @@
 int main(int argc, char ** argv)
 {
     Settings settings;
-    settings.set_print_regularity(1);
+    settings.set_print_regularity(100);
     settings.set_max_iterations(100000);
-    settings.set_grammar_size(10);
-    settings.set_run_iterations(4);
-    settings.set_rule_mutation_probability(50);
+    settings.set_grammar_size(5);
+    settings.set_run_iterations(7);
+    settings.set_rule_mutation_probability(100);
     settings.set_rulepart_insdel_probability(100);
-    settings.set_rulerow_insdel_probability(200);
+    settings.set_rulerow_insdel_probability(100);
 
     Universe universe(settings);
-    universe.init_train(settings, 800, 1500);
+    universe.init_train(settings, 25, 25);
     universe.run(settings);
 
     return 0;
--- settings.hpp
+++ settings.hpp
@@ -6,11 +6,11 @@
 
 struct Settings
 {
-    size_t population_size;  //amount of concurrent systems per generation
-    size_t grammar_size;     //variety of grammar to generate (3 would be A B C)
-    size_t run_iterations;   //how many iterations should the system run during rewrite stage
-    size_t max_iterations;   //how many total iterations should we do
-    size_t print_regularity; //how many iterations between printing current status
+    size_t population_size;
+    size_t grammar_size;
+    size_t run_iterations;
+    size_t max_iterations;
+    size_t print_regularity;
 
     Rng rng;
 
@@ -24,42 +24,50 @@ struct Settings
     rng{}
     {}
     
+    //amount of concurrent systems per generation
     void set_population_size(const size_t population_size)
     {
         this->population_size = population_size;
     }
 
+    //variety of grammar to generate (3 would be A B C)
     void set_grammar_size(const size_t grammar_size)
     {
         this->grammar_size = grammar_size;
         rng.grammar_dis.param(decltype(rng.grammar_dis)::param_type(0, grammar_size));
     }
 
+    //how many iterations should the system run during rewrite stage
     void set_run_iterations(const size_t run_iterations)
     {
         this->run_iterations = run_iterations;
     }
-
+    
+    //how many total iterations should we do
     void set_max_iterations(const size_t max_iterations)
     {
         this->max_iterations = max_iterations;
     }
-
+    
+    //how many iterations between printing current status
     void set_print_regularity(const size_t print_regularity)
     {
         this->print_regularity = print_regularity;
     }
 
+    //how often to mutate a piece of a rule
     void set_rule_mutation_probability(const size_t rule_mutation_probability)
     {
         rng.mutate_dis.param(decltype(rng.mutate_dis)::param_type(0, rule_mutation_probability));
     }
 
+    //how often should we insert or delete a piece of a rule
     void set_rulepart_insdel_probability(const size_t rule_insdel_probability)
     {
         rng.mutate_ins_dis.param(decltype(rng.mutate_ins_dis)::param_type(0, rule_insdel_probability));
     }
 
+    //how often should we create or delete a rule
     void set_rulerow_insdel_probability(const size_t rulerow_insdel_probability)
     {
         rng.mutate_rins_dis.param(decltype(rng.mutate_rins_dis)::param_type(0, rulerow_insdel_probability));
